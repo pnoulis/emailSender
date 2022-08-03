4bd1769f0f836912ac4134f503e4289fe75986c4
pavlos noulis <pavlos.noulis@gmail.com>
2022-08-03 13:35:25 +0300

    main form cerated

---
+++ emailSender.dpr
@@ -0,0 +1,14 @@
+﻿program emailSender;
+
+uses
+  Vcl.Forms,
+  main in 'src\main.pas' {FRMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFRMain, FRMain);
+  Application.Run;
+end.
---
+++ src/main.pas
@@ -0,0 +1,26 @@
+﻿unit main;
+
+interface
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs;
+
+type
+  TFRMain = class(TForm)
+    private
+    public
+  end;
+
+var
+  FRMain: TFRMain;
+
+implementation
+{$R *.dfm}
+end.
